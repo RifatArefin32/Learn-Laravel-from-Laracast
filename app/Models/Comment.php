e49909175ad684b35a6566599522f4e7adf04974
RifatArefin32 <arefinrifat786@gmail.com>
2024-11-21 09:53:36 +0600

    Update Comment class with relations

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['description'];
+
+    // Define relationships
+    // Many to One with Post model
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+
+    // Many to One with User model
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title', 'description'];
+
+    // Add relationship
+
+    // Many to One with User class
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    // One to Many with Comment class
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }   
 }
