f181ea2fb4f854f7c976db7b548288f0c4424f4b
RifatArefin32 <arefinrifat786@gmail.com>
2024-11-21 10:01:47 +0600

    Create comment factory

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Post;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
@@ -17,7 +19,9 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'description' => fake()->sentence(),
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
         ];
     }
 }
