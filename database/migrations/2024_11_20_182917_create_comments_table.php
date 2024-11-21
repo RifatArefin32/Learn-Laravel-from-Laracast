2eacd062131358c0666fe1db7af24a3b391ba5c2
RifatArefin32 <arefinrifat786@gmail.com>
2024-11-21 10:00:05 +0600

    Create comment migration and Post factory

--- database/migrations/2024_11_20_182917_create_comments_table.php
+++ database/migrations/2024_11_20_182917_create_comments_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Post;
+
 
 return new class extends Migration
 {
@@ -14,6 +17,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('description');
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Post::class)->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
