48e80db696f9bbfcb21361b609894248642053be
RifatArefin32 <arefinrifat786@gmail.com>
2024-11-24 21:54:32 +0600

    Update jobs blade

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,5 +1,28 @@
 <x-layout>
     <x-slot:heading> Jobs </x-slot:heading>
-    <x-slot:page_title> Jobs </x-slot:page_title>
-    <h1> All Jobs </h1>
+    <x-slot:page_title> All Jobs </x-slot:page_title>
+    @php
+        // dd($jobs);
+    @endphp
+    @if (!$jobs->isEmpty())
+        @foreach ($jobs as $job)
+            <div
+                class="border border-gray-300 rounded-lg shadow-sm p-4 my-4 cursor-pointer 
+            hover:shadow-lg transition duration-300">
+                <a href="/jobs/{{ $job->id }}">
+                    <div class="text-sm text-blue-500">
+                        <p> Company: {{ $job->employer->name }} </p>
+                    </div>
+                    <div class="text-xl mt-2">
+                        <p>{{ $job->title }}</p>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    @else
+        <div class="flex items-center justify-center my-10 text-xl">
+            <h1>No jobs available</h1>
+        </div>
+    @endif
+
 </x-layout>
