1c2aacabf3966d666aa5ec4c05c8b8d283f0c2fc
RifatArefin32 <arefinrifat786@gmail.com>
2024-11-24 22:20:20 +0600

    Prevent lazy loading to the entire site

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This line should be added if I want to prevent 
+        // lazy loading to the entire site
+
+        // Model::preventLazyLoading(true);
     }
 }
